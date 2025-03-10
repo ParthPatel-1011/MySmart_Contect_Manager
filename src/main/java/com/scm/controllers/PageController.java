1867a8bfd442cac90bd89164bb04ab3fd6d17bca
Parth Patel <parthpatel101551@gmail.com>
2025-03-10 23:06:39 +0530

    most repllayed 1

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,12 +1,21 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.forms.UserForm;
+
 
 @Controller
 public class PageController {
+
+    @Autowired
+    // private UserService userService;
 
     @RequestMapping("/home")
     public String home(Model model){
@@ -43,7 +52,25 @@ public class PageController {
 
     // Signup route 
     @RequestMapping("/register")
-    public String register(){
+    public String register(Model model){
+        // defoult data also you can add
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
         return "register";
     }
+
+    // prosesing register
+    @RequestMapping(value = "/do-register", method=RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm) {
+        // fetch form data
+        // validate form data
+        // save to database
+
+        // user service
+
+        // message : registration succsesfull
+        //redirecct another page
+        return "redirect:/register";
+    }
+    
 }
